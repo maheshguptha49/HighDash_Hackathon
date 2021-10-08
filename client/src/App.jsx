79056263e4c2b9303313f61f25050142c6a5f283
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2021-10-08 19:46:51 +0530

    signup & login

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
+
+function App() {
+  return (
+    <div >
+      <Login />
+     <Signup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Login/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import styles from './Login.module.css'
+import { createTheme , ThemeProvider} from '@mui/material/styles';
+import { purple,cyan } from '@mui/material/colors';
+const theme = createTheme({
+   palette: {
+    primary: {
+      main: cyan[500],
+    },
+    secondary:{
+      main: cyan[500],
+    }, 
+  },
+});
+
+const Login = () => {
+    return (
+        <div className={styles.mainDivLogin}>
+            
+            <div className={styles.boxInputField}>
+                <img src="https://res.cloudinary.com/rsbrsb/image/upload/v1633696870/WhatsApp_Image_2021-10-08_at_4.56.50_PM_lvvvq6.jpg" alt="" className={styles.boxInputFieldImg} />
+            <ThemeProvider theme={theme}>
+                <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off" >
+                    <TextField label="Email" color="secondary" focused className={styles.textInputField}
+                    inputProps={{ style: { fontFamily: 'Arial', color: 'cyan'}}}/>
+                    <TextField  label="Password" color="secondary" focused className={styles.textInputField} inputProps={{ style: { fontFamily: 'Arial', color: 'cyan'}}}/>
+                </Box>
+                <div className={styles.btnLoginDiv}>
+                    <button className={styles.loginBtn}><span /><span /><span /><span /> Login</button>
+                </div>
+                
+            </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Login
