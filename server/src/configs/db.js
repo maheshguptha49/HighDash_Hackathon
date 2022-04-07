0797c36a9a52dc8b13f8583177b14fb72de4c4b3
Deependra Negi <81069961+Deependra-Negi@users.noreply.github.com>
2022-04-07 13:09:01 +0530

    db url changed

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // const MongoURL = process.env.MONGO_URL || "http://localhost:27017/";
-const MongoURL = "http://localhost:27017/";
+const MongoURL = "http://localhost:3032/";
 
 const connect = () => mongoose.connect(MongoURL);
 
