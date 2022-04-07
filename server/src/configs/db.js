54c6399c3860760d7173f16ccbc3ce5143d70181
Deependra Negi <81069961+Deependra-Negi@users.noreply.github.com>
2022-04-07 13:01:56 +0530

    db url removed
    undo these changes after a week

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MongoURL = process.env.MONGO_URL || "http://localhost:27017/";
+// const MongoURL = process.env.MONGO_URL || "http://localhost:27017/";
+const MongoURL = "http://localhost:27017/";
 
 const connect = () => mongoose.connect(MongoURL);
 
