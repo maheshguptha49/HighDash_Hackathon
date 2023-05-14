2e39ba0ba33076704e945105c02b4dbc1ea3e3f2
Rushikesh Bambadkar <82999542+Rushikesh2503@users.noreply.github.com>
2023-05-14 18:37:32 +0530

    Update db.js

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// const MongoURL = process.env.MONGO_URL || "http://localhost:27017/";
-const MongoURL = "http://localhost:3032/";
+ const MongoURL = process.env.MONGO_URL || "http://localhost:27017/";
+//const MongoURL = "http://localhost:3032/";
 
 const connect = () => mongoose.connect(MongoURL);
 
